84f68515eb25f280240758409af4aac097709cd2
Penkar <jspenkar@gmail.com>
2018-11-30 18:06:30 -0600

    Scrollings events and a product section added.

--- src/lib/ECommerceSite.react.js
+++ src/lib/ECommerceSite.react.js
@@ -2,6 +2,8 @@ import React from 'react';
 import HeaderRow from './HeaderRow';
 import SlideBackground from './SlideBackground';
 import FloatingTitle from './FloatingTitle';
+import ProductSection from './ProductSection';
+
 import HashUtilities from '../utilities/hash';
 type Props = {hash:Object}
 
@@ -12,17 +14,31 @@ export default class EcommerceSite extends React.Component<Props, {}> {
   }
   componentDidMount() {
     window.addEventListener("hashchange", ()=>this.hashChange());
+    window.addEventListener("scroll", ()=>this.scrollChange());
   }
   render() {
+    const view = this.state.hash.view;
     return (
-      <div className="ECommerceSite">
+      <div className={`ECommerceSite ${view ? "open" : "" }`}>
         <HeaderRow />
-        <SlideBackground view={this.state.hash.view} />
-        <FloatingTitle view={this.state.hash.view} />
+        <SlideBackground view={view} />
+        <FloatingTitle view={view} />
+        <ProductSection view={view} />
       </div>
     );
   }
+
   hashChange = () => {
     this.setState({hash: HashUtilities.hashToJson()});
+  }
+  scrollChange = () => {
+    const hash = this.state.hash;
+    if([undefined, "beachwear"].indexOf(hash.view) === -1) return null;
+    if(window.pageYOffset > (window.innerHeight - 100)) {
+      hash.view = "beachwear";
+    } else {
+      hash.view = undefined;
+    }
+    return this.setState({hash});
   }
 }
---
+++ src/lib/ProductSection.react.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class ProductSection extends React.Component {
+  render() {
+    return (
+      <div className="ProductSection">
+      </div>
+    )
+  }
+}
